4a638d7132b7a197606577053188785cbb465007
shuhaoLIN <1432427815@qq.com>
2019-11-30 21:51:24 +0800

    对Viewpager添加了支持无限滑动、自动滚动、按住不动或滑动不动、点击事件的效果

--- 02广告条效果_viewpager/src/main/java/com/example/viewpager/MainActivity.java
+++ 02广告条效果_viewpager/src/main/java/com/example/viewpager/MainActivity.java
@@ -1,19 +1,21 @@
 package com.example.viewpager;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +37,19 @@ public class MainActivity extends AppCompatActivity {
             "图片二",
             "图片三",
             "图片四"
+    };
+
+    private  int prePosition = 0; //这个prePosition是针对group中第几个子View定义的
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            int item = viewpager.getCurrentItem() + 1 ;
+            viewpager.setCurrentItem(item);
+
+            handler.sendEmptyMessageDelayed(0, 2000);
+        }
     };
 
     @Override
@@ -59,9 +74,80 @@ public class MainActivity extends AppCompatActivity {
             ImageView imageView = new ImageView(this);
             imageView.setBackgroundResource(imageIds[i]);
             imageViews.add(imageView);
+
+            //添加点（用图片来做）
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(15,15);
+            ImageView point = new ImageView(this);
+            point.setBackgroundResource(R.drawable.point_selector);
+            if(i == prePosition){
+                point.setEnabled(true);
+            }else{
+                point.setEnabled(false);
+                params.leftMargin = 15;
+            }
+            point.setLayoutParams(params);
+            point_group.addView(point);
         }
 
         viewpager.setAdapter(new MyPagerAdapter());
+        //设置监听viewpager的页面改变
+        viewpager.addOnPageChangeListener(new MyOnPageChangeListener());
+        textview.setText(imageDec[prePosition]);
+
+         int currentItem = Integer.MAX_VALUE / 2 - Integer.MAX_VALUE / 2 % imageViews.size();
+        //使得第一个item是中间item，那么就可以保证能够左右滑动了
+        viewpager.setCurrentItem(currentItem);
+
+        handler.sendEmptyMessageDelayed(0, 2000);
+    }
+    class MyOnPageChangeListener implements ViewPager.OnPageChangeListener{
+        /**
+         * 当页面滚动了回调这个方法
+         * @param position 当前页面的位置
+         * @param positionOffset 滑动页面的百分比
+         * @param positionOffsetPixels 在屏幕上滑动的像素
+         */
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        /**
+         * 当某个页面被选中的时候回调
+         * @param position 被选中的页面的位置
+         */
+        @Override
+        public void onPageSelected(int position) {
+            int realPosition = position % imageViews.size();
+
+            //设置对应页面的文本信息
+            textview.setText(imageDec[realPosition]);
+            //把上一次的高亮设置为灰色
+            point_group.getChildAt(prePosition).setEnabled(false); // 因为这个是针对这个group的，所以要用realPosition
+            //这一次选中的为高亮
+            point_group.getChildAt(realPosition).setEnabled(true);
+
+            prePosition = realPosition;
+        }
+
+        /**
+         * 当页面的滚动状态变化的时候回调
+         * 静止 --》 滑动
+         * 滑动 --》 静止
+         * 静止 --》 拖拽
+         * @param state
+         */
+        @Override
+        public void onPageScrollStateChanged(int state) {
+            if (state == ViewPager.SCROLL_STATE_DRAGGING){
+                handler.removeCallbacksAndMessages(null);
+            }else if(state == ViewPager.SCROLL_STATE_SETTLING){
+
+            }else if (state == ViewPager.SCROLL_STATE_IDLE){
+                handler.removeCallbacksAndMessages(null);
+                handler.sendEmptyMessageDelayed(0, 2000);
+            }
+        }
     }
     class MyPagerAdapter extends PagerAdapter{
 
@@ -71,7 +157,7 @@ public class MainActivity extends AppCompatActivity {
          */
         @Override
         public int getCount() {
-            return imageViews.size();
+            return Integer.MAX_VALUE;
         }
 
         /**
@@ -82,8 +168,43 @@ public class MainActivity extends AppCompatActivity {
          */
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            ImageView imageView = imageViews.get(position);
-            Log.e(TAG, "instantiateItem: ==" + position + "  iamgeview == " + imageView);
+            int realPosition = position % imageViews.size();
+            ImageView imageView = imageViews.get(realPosition);
+            Log.e(TAG, "instantiateItem: ==" + position + "  iamgeview == " + imageView + " -- realPosition == " + realPosition);
+
+            //设置点击事件
+            imageView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    switch(event.getAction()){
+                        case MotionEvent.ACTION_DOWN: //手指按下去
+                            Log.e(TAG, "onTouch: down");
+                            handler.removeCallbacksAndMessages(null);
+                            break;
+                        case MotionEvent.ACTION_MOVE: //手指移动
+                            break;
+                        case MotionEvent.ACTION_CANCEL:
+                            break;
+                        case MotionEvent.ACTION_UP: //手指离开
+                            Log.e(TAG, "onTouch: UP");
+                            handler.sendEmptyMessageDelayed(0, 2000);
+                            break;
+
+                    }
+                    return false;
+                }
+            });
+            //让View去携带信息
+            imageView.setTag(position);
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = (int) v.getTag() % imageViews.size();
+
+                    String text = imageDec[position];
+                    Toast.makeText(MainActivity.this, "text ==" + text, Toast.LENGTH_SHORT).show();
+                }
+            });
             container.addView(imageView);
             return imageView;
         }
